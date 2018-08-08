fdb1eadb479a3573ff80d87ded67c2916059a96c
Timbo <mcloha@gmail.com>
2018-08-08 21:50:41 +0300

    second commit

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // MySQL import
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
 // App config
 const app = express();
 const port = 3000;
 
 // MySQL config
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
-});
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'test'
+// });
 
 // Body parser config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // MySQL connect
-con.connect((err) => {
-    if (err) throw err;
-    console.log('connected');
-})
+// con.connect((err) => {
+//     if (err) throw err;
+//     console.log('connected');
+// })
 
 app.post('/api/xfone', (req, res) => {
     const { name, age } = req.body.data;
@@ -40,13 +40,13 @@ app.listen(port, () => {
 })
 
 // MySQL query
-mySqlQuery = (name, age) => {
-    const sql = `INSERT INTO users (name, age) VALUES ('${name}', '${age}')`;
-    con.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    });
-}
+// mySqlQuery = (name, age) => {
+//     const sql = `INSERT INTO users (name, age) VALUES ('${name}', '${age}')`;
+//     con.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// }
 
 
 
